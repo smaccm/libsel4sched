364553d46671896209771562739323422367bdbd
Anna Lyons <Anna.Lyons@nicta.com.au>
2013-09-16 16:08:06 +1000

    update for bindable api change

--- src/sched.c
+++ src/sched.c
@@ -5,14 +5,15 @@
 
 
 vka_object_t
-sched_alloc_configure(seL4_SchedControl sched_control, vka_t *vka, seL4_SchedParams params) 
+sched_alloc_configure(seL4_SchedControl sched_control, vka_t *vka, seL4_SchedParams params,
+        bool bindable) 
 {
     vka_object_t sc = {0};
 
     int error = vka_alloc_sched_context(vka, &sc);
     assert(error == 0);
 
-    error = sched_configure(sched_control, sc.cptr, params);
+    error = sched_configure(sched_control, sc.cptr, params, bindable);
     assert(error == 0);
 
     return sc;
